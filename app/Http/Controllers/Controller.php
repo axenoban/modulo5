042ef3d7a7fb396d005ea7394e657e7d6ffd8f36
axenoban <147664740+axenoban@users.noreply.github.com>
2026-07-23 23:16:52 -0400

    Revise API documentation and server details
    Updated API documentation description and server description.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,15 +8,12 @@ use OpenApi\Annotations as OA;
  * @OA\Info(
  *      version="1.0.0",
  *      title="API de Mantenimiento TI",
- *      description="Documentación oficial de la API REST para el sistema de gestión de mantenimiento TI.",
- *      @OA\Contact(
- *          email="soporte@tu-dominio.com"
- *      )
+ *      description="Documentación de la API REST"
  * )
- * 
+ *
  * @OA\Server(
  *      url=L5_SWAGGER_CONST_HOST,
- *      description="Servidor de Producción (Railway)"
+ *      description="Servidor de Producción Railway"
  * )
  */
 abstract class Controller
