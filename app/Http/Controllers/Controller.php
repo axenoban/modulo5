178bc1e28b6b10e15af778548b0846f7addb7904
axenoban <147664740+axenoban@users.noreply.github.com>
2026-07-24 00:24:45 -0400

    Refactor OpenAPI annotations to attributes

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,26 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use OpenApi\Annotations as OA;
+use OpenApi\Attributes as OA;
 
-/**
- * @OA\OpenApi(
- *      @OA\Info(
- *          version="1.0.0",
- *          title="API Módulo de Mantenimiento",
- *          description="Documentación oficial de los microservicios de mantenimiento."
- *      ),
- *      @OA\Server(
- *          url="https://modulo5-production.up.railway.app/api",
- *          description="Servidor de Producción Railway"
- *      ),
- *      @OA\SecurityScheme(
- *          securityScheme="bearerAuth",
- *          type="http",
- *          scheme="bearer"
- *      )
- * )
- */
+#[OA\OpenApi(
+    info: new OA\Info(
+        version: "1.0.0",
+        title: "API Módulo de Mantenimiento",
+        description: "Documentación oficial de los microservicios de mantenimiento."
+    )
+)]
+#[OA\Server(
+    url: "https://modulo5-production.up.railway.app/api",
+    description: "Servidor de Producción Railway"
+)]
+#[OA\SecurityScheme(
+    securityScheme: "bearerAuth",
+    type: "http",
+    scheme: "bearer"
+)]
 abstract class Controller
 {
     //
