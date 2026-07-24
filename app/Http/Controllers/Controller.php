47a530398399baa1468265888e448d2643ecb503
axenoban <147664740+axenoban@users.noreply.github.com>
2026-07-23 23:21:04 -0400

    Add OpenAPI server details to Controller.php
    Updated OpenAPI annotations to include server information.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,15 +5,16 @@ namespace App\Http\Controllers;
 use OpenApi\Annotations as OA;
 
 /**
- * @OA\Info(
- *      version="1.0.0",
- *      title="API de Mantenimiento TI",
- *      description="Documentación de la API REST"
- * )
- *
- * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="Servidor de Producción Railway"
+ * @OA\OpenApi(
+ *      @OA\Info(
+ *          version="1.0.0",
+ *          title="API de Mantenimiento TI",
+ *          description="Documentación de la API REST"
+ *      ),
+ *      @OA\Server(
+ *          url=L5_SWAGGER_CONST_HOST,
+ *          description="Servidor de Producción Railway"
+ *      )
  * )
  */
 abstract class Controller
